3d4d71b761a044735f6a740116c8e351940a45e4
System Administrator <root@Macbooks-MBP.hitronhub.home>
2019-04-12 22:36:18 -0600

    #1 added comments of how to add the config file to the project and how to run it

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 app.config.from_envvar('APP_SETTINGS')
 
 
+# must run export APP_SETTINGS=/path/settings.cfg first
+# then python app.py
+
 @app.route('/')
 def hello_world():
     return 'Hello World!' + app.config['ENV']
