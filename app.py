e4847ff847fc914a6090edd79902b31d6b5f8d2a
System Administrator <root@Macbooks-MBP.hitronhub.home>
2019-04-12 22:34:51 -0600

    #1 created settings.cfg file to store all the configuration files

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_envvar('APP_SETTINGS')
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!' + app.config['ENV']
 
 
 if __name__ == '__main__':
