0b45036647595978e944bce6dd4e3f9d762098cd
Pem Taira <jose_taira@yahoo.com>
2019-10-04 22:11:48 -0600

    #6 Improve init.sh to be more of a double-click easy installer

--- scripts/connect-to-wifi.py
+++ scripts/connect-to-wifi.py
@@ -10,14 +10,25 @@ from wireless import Wireless
 from dotenv import load_dotenv
 
 import os
+import logging
+import traceback
+
+CURR_FILE_PATH=os.path.dirname(os.path.realpath(__file__))
+LOG_FILE=CURR_FILE_PATH + '/connect-to-wifi.log'
+DOTENV_PATH = CURR_FILE_PATH + '/../.env'
+
+# Load .env
+load_dotenv(DOTENV_PATH)
+# Create logfile
+logging.basicConfig(filename=LOG_FILE)
 
 def connect():
-    DOTENV_PATH = os.path.dirname(os.path.realpath(__file__)) + '/../.env'
-
-    load_dotenv(DOTENV_PATH)
-
     WIFI_SSID = os.getenv('WIFI_SSID')
     WIFI_PASSPHRASE = os.getenv('WIFI_PASSPHRASE')
+
+    if not WIFI_SSID or not WIFI_PASSPHRASE:
+        print('WIFI_SSID or WIFI_PASSPHRASE not set in .env. NOOP')
+        return
 
     wire = Wireless()
 
@@ -29,15 +40,15 @@ def connect():
         is_connected = wire.connect(ssid=WIFI_SSID, password=WIFI_PASSPHRASE)
 
         if is_connected:
-            print('Successfully connected to [' + os.getenv('WIFI_SSID') + ']')
+            print('Successfully connected to [' + WIFI_SSID + ']')
         else:
-            print('Failed to connect to [' + os.getenv('WIFI_SSID') + ']')
+            print('Failed to connect to [' + WIFI_SSID + ']')
             tries = tries + 1
     
     if not is_connected:
-        raise Exception('Unable to connect to the wifi after 3 tries')
+        raise Exception('Unable to connect to [' + WIFI_SSID + '] after 3 tries')
 
 try:
     connect()
-except:
-    print('No driver to connect with or failed to connect too many times')
+except Exception as e:
+    logging.error(traceback.format_exc())
--- scripts/init.sh
+++ scripts/init.sh
@@ -5,57 +5,132 @@
 # - init.sh
 # - requirements.txt
 
-if [ -z "$OPEN_KITCHEN_PATH" ]
+CURRENT_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ -d $OPEN_KITCHEN_SCRIPTS_PATH ]
 then
-    OPEN_KITCHEN_PATH=/var/www
+    echo "[SKIP] Creating $OPEN_KITCHEN_SCRIPTS_PATH"
+else
+    echo "[EXEC] Creating $OPEN_KITCHEN_SCRIPTS_PATH"
+    mkdir $OPEN_KITCHEN_SCRIPTS_PATH
+    echo "[EXEC] Copying contents of $CURRENT_SCRIPTS_DIR to $OPEN_KITCHEN_SCRIPTS_PATH"
+    cp $CURRENT_SCRIPTS_DIR/* $OPEN_KITCHEN_SCRIPTS_PATH
+    chmod +x $OPEN_KITCHEN_SCRIPTS_PATH/*.sh
 fi
 
-echo "[APT-GET] Installing build dependencies"
+# Create the logs folder:
+if [ ! -d ~/logs ]
+    mkdir ~/logs
+fi
+
+echo "[EXEC] Try Installing cron, git, curl and build dependencies"
 sudo apt-get update -y
 sudo apt-get install cron git curl build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
 
-sudo systemctl enable cron
-
 # Create and clone the repository
-echo "[GIT] Cloning repository to $OPEN_KITCHEN_PATH"
-git clone https://github.com/open-kitchen/open-kitchen $OPEN_KITCHEN_PATH
-git checkout $OPEN_KITCHEN_PATH
-
-if [ [ ! -f "$OPEN_KITCHEN_PATH/.env" ] && [ -f /scripts/.env ] ]
+if [ -d $OPEN_KITCHEN_PATH ]
 then
-    echo "Copying .env from /scripts to $OPEN_KITCHEN_PATH"
-    cp /scripts/.env $OPEN_KITCHEN_PATH/.env
+    echo "[SKIP] Cloning repository to $OPEN_KITCHEN_PATH, branch: $OPEN_KITCHEN_BRANCH, from: $OPEN_KITCHEN_REPO_URL"
 else
-    echo ".env already exist or cannot be copied from /scripts"
+    echo "[EXEC] Cloning repository to $OPEN_KITCHEN_PATH, branch: $OPEN_KITCHEN_BRANCH, from: $OPEN_KITCHEN_REPO_URL"
+    git clone -b $OPEN_KITCHEN_BRANCH $OPEN_KITCHEN_REPO_URL $OPEN_KITCHEN_PATH
+    git checkout $OPEN_KITCHEN_PATH
+fi
+
+if [ -d $OPEN_KITCHEN_PATH ]
+then
+    # Attempt to copy only if the .env does not exist
+    if [ -f "$OPEN_KITCHEN_PATH/.env" ]
+    then
+        echo ".env already exist or cannot be copied from /scripts"
+    else
+        if [ -f $CURRENT_SCRIPTS_DIR/.env ]
+        then
+        else
+            echo "Copying .env from /scripts to $OPEN_KITCHEN_PATH"
+            cp /scripts/.env $OPEN_KITCHEN_PATH/.env
+        fi
+    fi
+else
+    echo "[SKIP] Copying .env from $OPEN_KITCHEN_SCRIPTS_PATH to $OPEN_KITCHEN_PATH"
+    echo "[SKIP] Copying settings.cfg from $OPEN_KITCHEN_SCRIPTS_PATH to $OPEN_KITCHEN_PATH"
+    echo "------ Path: $OPEN_KITCHEN_PATH does not exist"
 fi
     
+if type python3 && (python3 --version | grep 3.7)
+then
+    INSTALLED_PYTHON=python3 --version
+    echo "[SKIP] Installing Python 3.7.2"
+    echo "------ $INSTALLED_PYTHON already installed"
+else
+    # Install Python3.7.2 and make an alias to it
+    echo "[EXEC] Installing Python 3.7.2"
+    wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tar.xz
+    tar xf Python-3.7.2.tar.xz
+    cd Python-3.7.2
+    ./configure
+    make -j 4
+    sudo make altinstall
 
-# Install Python3.7.2 and make an alias to it
-echo "Installing python 3.7.2"
-wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tar.xz
-tar xf Python-3.7.2.tar.xz
-cd Python-3.7.2
-./configure
-make -j 4
-sudo make altinstall
+    echo "alias python3='python3.7'" >> ~/.bashrc
+fi
 
 # .bashrc changes
-echo "Adding .bashrc changes"
-echo "alias python3='python3.7'" >> ~/.bashrc
-echo "export APP_SETTINGS=$OPEN_KITCHEN_PATH/setting.cfg"
-echo "/scripts/startup.sh &" >> ~/.bashrc
+if grep "export APP_SETTINGS" ~/.bashrc
+then
+    echo "[SKIP] Adding APP_SETTINGS export to .bashrc"
+else
+    echo "[EXEC] Adding APP_SETTINGS export to .bashrc"
+    echo "export APP_SETTINGS=$OPEN_KITCHEN_PATH/setting.cfg" >> ~/.bashrc
+fi
+
+if grep "$OPEN_KITCHEN_SCRIPTS_PATH/startup.sh" ~/.bashrc
+then
+    echo "[SKIP] Adding $OPEN_KITCHEN_SCRIPTS_PATH/startup.sh to .bashrc"
+else
+    echo "[EXEC] Adding $OPEN_KITCHEN_SCRIPTS_PATH/startup.sh to .bashrc"
+    echo "$OPEN_KITCHEN_SCRIPTS_PATH/startup.sh &" >> ~/.bashrc
+fi
 
 # Will install python3
-echo "Installing pip3"
-wget https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
+if type pip3
+then
+    echo "[SKIP] Installing pip3"
+    echo "------ pip3 already exists"
+else
+    echo "[EXEC] Installing pip3"
+    wget https://bootstrap.pypa.io/get-pip.py
+    python3 get-pip.py
+    echo "[ OK ] pip3 Installed"
+fi
 
 # Install all python dependencies:
-echo "[Python] Installing python dependencies"
-pip3 --timeout 1000 --retries 50 install -r /scripts/requirements.txt
+if [ -f $OPEN_KITCHEN_PATH/requirements.txt ]
+then
+    echo "[EXEC] Trying to install open-kitchen python dependencies via pip3"
+    pip3 --timeout 1000 --retries 50 install -r $OPEN_KITCHEN_PATH/requirements.txt
+    echo "[ OK ] Python dependencies installed"
+else
+    echo "[SKIP] Trying to install open-kitchen python dependencies via pip3"
+    echo "------ Could not find $OPEN_KITCHEN_PATH/requirements.txt"
+fi
 
 # Will be used to connect to wifi:
-# pip3 install wireless packaging
+if crontab -l | grep health-check.sh
+then
+    echo "[SKIP] Register cron health-check"
+else
+    echo "[EXEC] Register cron health-check"
+    (crontab -l 2>/dev/null; echo "*/1 * * * * $OPEN_KITCHEN_SCRIPTS_PATH/health-check.sh") | crontab -
+    sudo systemctl enable cron
+    sudo service cron start
+fi
 
-(sudo crontab -l 2>/dev/null; echo "*/1 * * * * /scripts/health-check.sh") | sudo crontab -
-sudo service cron start
+if ps -ef | grep "[a]pp.py"
+then
+    echo "[SKIP] Executing $OPEN_KITCHEN_SCRIPTS_PATH/startup.sh"
+    echo "------ Already running"
+else
+    echo "[EXEC] Executing $OPEN_KITCHEN_SCRIPTS_PATH/startup.sh"
+    $OPEN_KITCHEN_SCRIPTS_PATH/startup.sh &
+fi
