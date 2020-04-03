936b0caf24b96270475df0161c1635ed6741739e
NunchakusLei <jimmynunchakuslei@gmail.com>
2020-04-03 08:35:29 +0100

    Tested on Pi

--- i2c_com/i2c_util.py
+++ i2c_com/i2c_util.py
@@ -5,43 +5,45 @@ import time
 class I2CDevice:
     def __init__(self, address: int) -> None:
         self.addr = address
-    
+
     def set_address(self, address: int) -> None:
         self.addr = address
-        
+
     def _i2c_rw(self, msg: i2c_msg) -> None:
         with SMBus(1) as bus:
             bus.i2c_rdwr(msg)
-        
+
     def send(self, data: str or int) -> None:
         if type(data) is str:
             data = list(data)
             data = list(map(ord, data))
         elif type(data) is int:
-            data = [data & 0xff]
-        elif type(date) is list:
-            data = list(map(lambda x: int(x) & 0xff, data))
+            data = [data & 0xFF]
+        elif type(data) is list:
+            data = list(map(lambda x: int(x) & 0xFF, data))
         else:
             raise TypeError("Unsupport data type to send.")
-            
+
         cmd_msg = i2c_msg.write(self.addr, data)
         self._i2c_rw(cmd_msg)
-            
+
     def read(self, read_bytes: int = 1) -> str:
         request_msg = i2c_msg.read(self.addr, read_bytes)
         self._i2c_rw(request_msg)
-        
-        response = [chr(value) for value in request_msg]
-        return "".join(response)
-        
+
+        response = [value for value in request_msg]
+        return response
+
 
 if __name__ == "__main__":
     i2c_device_1 = I2CDevice(0x20)
-    
+
     cmd = input("Type in message to send (no longer than 32 charactors): ")
-    print(f"Sending message: \"{cmd}\" (Length: {len(cmd)})")
+    print(f'Sending message: "{cmd}" (Length: {len(cmd)})')
     i2c_device_1.send(cmd)
-    
+
     time.sleep(0.1)  # Give it sometime to recover
-    print(f"Receive message: \"{i2c_device_1.read(len(cmd))}\"")
-    
+    response = i2c_device_1.read(len(cmd))
+    response = [chr(value) for value in response]
+    response = "".join(response)
+    print(f'Receive message: "{response}"')
--- simulation/components/runner_hardware.py
+++ simulation/components/runner_hardware.py
@@ -1,3 +1,4 @@
+from i2c_com.i2c_util import I2CDevice
 from components import ComponentSim
 from components.runner_sim import RunnerSim
 
@@ -6,18 +7,22 @@ class ComponentHardware(ComponentSim):
     def __init__(self, address: int) -> None:
         # TODO: establish I2C communication
         self._i2c_device = I2CDevice(address)
-        self.log.info(f"Established {self.__class__.__name__}({hex(address)}) at I2C address {hex(address)}")
-        
+        self.log.info(
+            f"Established {self.__class__.__name__}({hex(address)}) at I2C address {hex(address)}"
+        )
+
     def stop(self):
         pass
-        
+
     def request(self, request_code: int, data: int) -> int:
         # TODO: send I2C message and read I2C responses
-        self._i2c_device.send(request_code, data) if data is not None
-        self._i2c_device.send(request_code) 
-        response = self._i2c_device.read(1)
+        if data is not None:
+            self._i2c_device.send([request_code, data])
+        else:
+            self._i2c_device.send(request_code)
+        response = self._i2c_device.read(1)[0]
         return response
-        
-        
+
+
 class RunnerHardware(ComponentHardware, RunnerSim):
     pass
